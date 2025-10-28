44fa03ae983e473352904c558729d99475534d3b
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2025-10-28 10:41:34 +0400

    Update task_2.1.ino

--- task_2.1.ino
+++ task_2.1.ino
@@ -1,19 +1,15 @@
-
-uint8_t lastButtonState = 0;
-
 void setup() {
-  DDRB &= ~(1 << PB4);
-  PORTB |= (1 << PB4);
-  DDRB |= (1 << PB5);
+  // put your setup code here, to run once:
+  DDRB |= (1 << DDB5);
+  DDRD &= ~(1 << DDD2);
 }
 
 void loop() {
-  uint8_t buttonState = PINB & (1 << PB4);
-  if (PINB & (1 << PB4)) {
-    if (buttonState && !lastButtonState){
-      PORTB ^= (1 << PB5);
-    }
+  // put your main code here, to run repeatedly:
+  if((PIND & B00000100) == 4){
+    PORTB |= (1 << PORTB5);
   }
-  lastButtonState = buttonState; 
-  delay(50);
+  else{
+    PORTB &= ~(1 << PORTB5);
+  }
 }
