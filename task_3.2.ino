bd171f0611c2416376a3b9ed6f968657037deaf7
Arif Aykhan Mammadli <79666195+arifaykhan@users.noreply.github.com>
2025-10-31 14:49:57 +0400

    Update task_3.2.ino

--- task_3.2.ino
+++ task_3.2.ino
@@ -4,14 +4,14 @@ void setup() {
   Serial.begin(9600);
   uint8_t res;
   asm volatile(
-    "ldi r16, 1 \n\t"
-    "ldi r17, 0 \n\t"
-    "ldi r18, 10 \n\t"
+    "ldi r16, 1 \n\t" //counter
+    "ldi r17, 0 \n\t" //running sum
+    "ldi r18, 11 \n\t" //limit
 
     "counter: \n\t"
     "add r17, r16 \n\t"
     "inc r16 \n\t"
-    "cp r17, r18 \n\t"
+    "cp r16, r18 \n\t"
     "brne counter \n\t"
     "mov %[res], r17 \n\t"
     : [res] "=r" (res)
